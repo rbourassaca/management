af85b5b013eff84285a2ddc80a942278b1a4faab
Raphael Bourassa <bourassa.raphael@outlook.com>
2024-04-15 12:06:01 -0400

    Affichage des informations de l'utilisateur connecté.

--- frontend/src/routes/dashboard/settings/profile/+page.svelte
+++ frontend/src/routes/dashboard/settings/profile/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
 
+	export let data;
 	export let form;
 
-	let firstName: string = form?.values?.firstName.toString() || '';
-	let lastName: string = form?.values?.lastName.toString() || '';
-	let email: string = form?.values?.email.toString() || '';
+	let firstName: string = data.user.firstName.toString() || '';
+	let lastName: string = data.user.lastName.toString() || '';
+	let email: string = data.user.email.toString() || '';
 	let currentPassword: string = '';
 	let newPassword: string = '';
 	let newPasswordConfirm: string = '';
@@ -14,7 +15,7 @@
 
 <h1 class="h1">Paramètres du profile</h1>
 <div class="card p-4">
-	<form action="#" use:enhance bind:this={formElement} class="flex flex-col gap-4">
+	<form action="#" method="post" use:enhance bind:this={formElement} class="flex flex-col gap-4">
 		<label class="label">
 			<span>Prénom</span>
 			{#if form !== null && form.error.firstName}
@@ -24,7 +25,7 @@
 				class={form !== null && form.error.firstName ? 'input input-error' : 'input'}
 				title="Prénom"
 				type="text"
-				placeholder="John"
+				placeholder={data.user.firstName}
 				name="firstName"
 				bind:value={firstName}
 			/>
@@ -38,7 +39,7 @@
 				class={form !== null && form.error.lastName ? 'input input-error' : 'input'}
 				title="Nom de famille"
 				type="text"
-				placeholder="Walter"
+				placeholder={data.user.lastName}
 				name="lastName"
 				bind:value={lastName}
 			/>
@@ -52,7 +53,7 @@
 				class={form !== null && form.error.email ? 'input input-error' : 'input'}
 				title="Email"
 				type="email"
-				placeholder="john@example.com"
+				placeholder={data.user.email}
 				autocomplete="email"
 				name="email"
 				bind:value={email}
