923cf53c677a50d854b39aaf3607cbd46200d4ef
Raphael Bourassa <bourassa.raphael@outlook.com>
2024-04-15 12:07:56 -0400

    Ajout padding bottom.

--- frontend/src/routes/dashboard/+layout.svelte
+++ frontend/src/routes/dashboard/+layout.svelte
@@ -7,7 +7,7 @@
 	<Sidebar />
 	<div class="flex flex-col gap-4 w-full">
 		<Breadcrumb trail={[{ name: 'Espace client' }]} />
-		<div class="pl-4 pr-4 flex flex-col gap-4">
+		<div class="pl-4 pr-4 pb-4 flex flex-col gap-4">
 			<slot />
 		</div>
 	</div>
