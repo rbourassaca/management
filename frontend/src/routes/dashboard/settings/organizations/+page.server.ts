22b53cde966b08ce0a461b902c5a4590705326bd
Raphael Bourassa <bourassa.raphael@outlook.com>
2024-04-17 19:17:53 -0400

    Ajout des informations des utilisateurs dans la requête.

--- frontend/src/routes/dashboard/settings/organizations/+page.server.ts
+++ frontend/src/routes/dashboard/settings/organizations/+page.server.ts
@@ -2,6 +2,7 @@ export const load = async ({ locals }) => {
 	try {
 		const organizations = await locals.pocketbase.collection('organizations').getFullList({
 			filter: `owners.id?="${locals.user.id}" || members.id?="${locals.user.id}"`,
+			expand: 'members, owners'
 		});
 		return {
 			organizations
