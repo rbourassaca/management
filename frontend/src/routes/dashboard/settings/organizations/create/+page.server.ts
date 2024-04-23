b6983ac88346d7acaca49f8ba24c0ef56f48e280
Raphael Bourassa <bourassa.raphael@outlook.com>
2024-04-23 15:37:24 -0400

    Redirection vers la page de modifications d'une organisation.

--- frontend/src/routes/dashboard/settings/organizations/create/+page.server.ts
+++ frontend/src/routes/dashboard/settings/organizations/create/+page.server.ts
@@ -3,13 +3,14 @@ import { redirect } from '@sveltejs/kit';
 export const actions = {
 	create: async ({ locals, request }) => {
 		const body = Object.fromEntries(await request.formData());
+		let record;
 		try {
-			await locals.pocketbase
+			record = await locals.pocketbase
 				.collection('organizations')
 				.create({ name: body.name, owners: locals.user.id });
 		} catch (err: any) {
 			return { success: false, error: err.data.data, values: { ...body } };
 		}
-		throw redirect(303, './');
+		throw redirect(303, `./edit/${record.id}`);
 	}
 };
