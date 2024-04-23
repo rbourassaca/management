d54514adf5e6d9ac67f4f61d552c585b55fefb91
Raphael Bourassa <bourassa.raphael@outlook.com>
2024-04-23 13:43:59 -0400

    Redirection lors de la création d'une organisation.

--- frontend/src/routes/dashboard/settings/organizations/create/+page.server.ts
+++ frontend/src/routes/dashboard/settings/organizations/create/+page.server.ts
@@ -1,11 +1,15 @@
+import { redirect } from '@sveltejs/kit';
+
 export const actions = {
 	create: async ({ locals, request }) => {
-		const body = Object.fromEntries(await request.formData())
+		const body = Object.fromEntries(await request.formData());
 		try {
-			await locals.pocketbase.collection('organizations').create({name: body.name, owners: locals.user.id})
-			return {success: true}
+			await locals.pocketbase
+				.collection('organizations')
+				.create({ name: body.name, owners: locals.user.id });
 		} catch (err: any) {
-			return {success: false, error: err.data.data, values: {...body}}
+			return { success: false, error: err.data.data, values: { ...body } };
 		}
+		throw redirect(303, './');
 	}
-}
+};
