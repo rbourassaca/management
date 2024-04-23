54cbcbe66522dd4f6f9c63ca5f9d8219620329e0
Raphael Bourassa <bourassa.raphael@outlook.com>
2024-04-23 17:36:24 -0400

    Create 1713908173_updated_organizations.js

---
+++ backend/pb_migrations/1713908173_updated_organizations.js
@@ -0,0 +1,44 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("p6b0efybpik4xb4")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "z2oku7tc",
+    "name": "name",
+    "type": "text",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": 64,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("p6b0efybpik4xb4")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "z2oku7tc",
+    "name": "name",
+    "type": "text",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": 32,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
