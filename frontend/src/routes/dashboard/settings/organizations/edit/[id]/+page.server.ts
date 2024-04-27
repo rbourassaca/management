2d6636cd414977c5c12a97e663315d2ceeb543a7
Raphael Bourassa <bourassa.raphael@outlook.com>
2024-04-27 14:36:32 -0400

    Utilisation du nouveau champ administrateurs, ajout des actions requises et ajustement du code de l'erreur.

--- frontend/src/routes/dashboard/settings/organizations/edit/[id]/+page.server.ts
+++ frontend/src/routes/dashboard/settings/organizations/edit/[id]/+page.server.ts
@@ -5,9 +5,9 @@ export const load = async ({ locals, params }) => {
 	try {
 		org = await locals.pocketbase
 			.collection('organizations')
-			.getOne(params.id, { expand: 'members, owners' });
+			.getOne(params.id, { expand: 'members, owners, administrators' });
 	} catch (err: any) {
-		throw error(404, err.response.message);
+		throw error(err.response.code, err.response.message);
 	}
 	return {
 		organization: org
@@ -19,6 +19,18 @@ export const actions = {
 		const formData = Object.fromEntries(await request.formData());
 		try {
 			await locals.pocketbase.collection('organizations').update(params.id, { ...formData });
+		} catch (err: any) {
+			return { success: false, error: err.data.data, values: { ...formData } };
+		}
+	},
+	addMembers: async ({ locals, params, request }) => {
+		const formData = Object.fromEntries(await request.formData());
+		try {
+			if (formData.owners) {
+				// Add owners
+			} else {
+				// Add members
+			}
 		} catch (err: any) {
 			return { success: false, error: err.data.data, values: { ...formData } };
 		}
