c85152365ab468e7d9e83f7ababa2659995dd4e7
Raphael Bourassa <bourassa.raphael@outlook.com>
2024-04-27 14:36:59 -0400

    Amélioration de la page pour gérer les organisations.

--- frontend/src/routes/dashboard/settings/organizations/edit/[id]/+page.svelte
+++ frontend/src/routes/dashboard/settings/organizations/edit/[id]/+page.svelte
@@ -15,6 +15,10 @@
 		{ name: data.organization.name }
 	];
 
+	let ownersEmails = '';
+	let administratorsEmails = '';
+	let membersEmails = '';
+
 	let name: string = '';
 
 	let editFormElement: HTMLFormElement;
@@ -26,12 +30,74 @@
 	<Accordion>
 		<AccordionItem>
 			<svelte:fragment slot="lead">
+				<iconify-icon icon="mdi:administrator" />
+			</svelte:fragment>
+			<svelte:fragment slot="summary">Propriétaires</svelte:fragment>
+			<svelte:fragment slot="content">
+				<Table
+					data={data.organization.expand?.owners}
+					fields={[
+						{ key: 'firstName', label: 'Prénom' },
+						{ key: 'lastName', label: 'Nom de famille' },
+						{ key: 'email', label: 'Courriel' }
+					]}
+					actions={data.organization.owners.includes(data.user.id)
+						? [
+								{
+									icon: 'clarity:trash-line',
+									name: 'Supprimer',
+									action: () => {
+										console.log('click');
+										//TODO: Handle action
+									}
+								}
+							]
+						: undefined}
+				/>
+				{#if data.organization.owners.includes(data.user.id)}
+					<Accordion>
+						<AccordionItem>
+							<svelte:fragment slot="lead">
+								<iconify-icon icon="mdi:administrator" />
+							</svelte:fragment>
+							<svelte:fragment slot="summary">Ajouter des propriétaires</svelte:fragment>
+							<svelte:fragment slot="content">
+								<form action="?/addMembers" method="post" class="flex flex-col gap-2">
+									<label for="emails">
+										Entrer les courriels des propriétaires à ajouter, séparé par une virgule.
+									</label>
+									<input type="hidden" name="owners" value={true} />
+									<div class="flex gap-1">
+										<input
+											class="input"
+											type="email"
+											multiple={true}
+											name="emails"
+											placeholder="john@example.com, susy@example.com"
+											bind:value={ownersEmails}
+										/>
+										<input
+											class="btn variant-filled-primary cursor-pointer"
+											type="submit"
+											value="Ajouter des administrateurs"
+											disabled={!(ownersEmails.length > 0)}
+										/>
+									</div>
+								</form>
+							</svelte:fragment>
+						</AccordionItem>
+					</Accordion>
+				{/if}
+			</svelte:fragment>
+		</AccordionItem>
+		<AccordionItem>
+			<svelte:fragment slot="lead">
 				<iconify-icon icon="clarity:administrator-line" />
 			</svelte:fragment>
 			<svelte:fragment slot="summary">Administrateurs</svelte:fragment>
 			<svelte:fragment slot="content">
 				<Table
-					data={data.organization.expand?.owners}
+					data={data.organization.expand?.administrators}
 					fields={[
 						{ key: 'firstName', label: 'Prénom' },
 						{ key: 'lastName', label: 'Nom de famille' },
@@ -48,6 +114,38 @@
 						}
 					]}
 				/>
+				<Accordion>
+					<AccordionItem>
+						<svelte:fragment slot="lead">
+							<iconify-icon icon="clarity:administrator-line" />
+						</svelte:fragment>
+						<svelte:fragment slot="summary">Ajouter des administrateurs</svelte:fragment>
+						<svelte:fragment slot="content">
+							<form action="?/addMembers" method="post" class="flex flex-col gap-2">
+								<label for="emails">
+									Entrer les courriels des administrateurs à ajouter, séparé par une virgule.
+								</label>
+								<input type="hidden" name="administrators" value={true} />
+								<div class="flex gap-1">
+									<input
+										class="input"
+										type="email"
+										multiple={true}
+										name="emails"
+										placeholder="john@example.com, susy@example.com"
+										bind:value={administratorsEmails}
+									/>
+									<input
+										class="btn variant-filled-primary cursor-pointer"
+										type="submit"
+										value="Ajouter des administrateurs"
+										disabled={!(administratorsEmails.length > 0)}
+									/>
+								</div>
+							</form>
+						</svelte:fragment>
+					</AccordionItem>
+				</Accordion>
 			</svelte:fragment>
 		</AccordionItem>
 		<AccordionItem>
@@ -64,6 +162,38 @@
 						{ key: 'email', label: 'Courriel' }
 					]}
 				/>
+				<Accordion>
+					<AccordionItem>
+						<svelte:fragment slot="lead">
+							<iconify-icon icon="clarity:user-line" />
+						</svelte:fragment>
+						<svelte:fragment slot="summary">Ajouter des membres</svelte:fragment>
+						<svelte:fragment slot="content">
+							<form action="?/addMembers" method="post" class="flex flex-col gap-2">
+								<label for="emails">
+									Entrer les courriels des membres à ajouter, séparé par une virgule.
+								</label>
+								<input type="hidden" name="owners" value={false} />
+								<div class="flex gap-1">
+									<input
+										class="input"
+										type="email"
+										multiple={true}
+										name="emails"
+										placeholder="john@example.com, susy@example.com"
+										bind:value={membersEmails}
+									/>
+									<input
+										class="btn variant-filled-primary cursor-pointer"
+										type="submit"
+										value="Ajouter des membres"
+										disabled={!(membersEmails.length > 0)}
+									/>
+								</div>
+							</form>
+						</svelte:fragment>
+					</AccordionItem>
+				</Accordion>
 			</svelte:fragment>
 		</AccordionItem>
 	</Accordion>
@@ -98,17 +228,19 @@
 	</form>
 </Panel>
 
-<h2 class="h2">Supprimer l'organisation</h2>
-<Panel>
-	<Accordion>
-		<AccordionItem>
-			<svelte:fragment slot="lead">
-				<iconify-icon icon="clarity:trash-line" />
-			</svelte:fragment>
-			<svelte:fragment slot="summary">Supprimer l'organisation</svelte:fragment>
-			<svelte:fragment slot="content">
-				<a href={`./${data.organization.id}/delete`} class="anchor">Supprimer l'organisation</a>
-			</svelte:fragment>
-		</AccordionItem>
-	</Accordion>
-</Panel>
+{#if data.organization.owners.includes(data.user.id)}
+	<h2 class="h2">Supprimer l'organisation</h2>
+	<Panel>
+		<Accordion>
+			<AccordionItem>
+				<svelte:fragment slot="lead">
+					<iconify-icon icon="clarity:trash-line" />
+				</svelte:fragment>
+				<svelte:fragment slot="summary">Supprimer l'organisation</svelte:fragment>
+				<svelte:fragment slot="content">
+					<a href={`./${data.organization.id}/delete`} class="anchor">Supprimer l'organisation</a>
+				</svelte:fragment>
+			</AccordionItem>
+		</Accordion>
+	</Panel>
+{/if}
